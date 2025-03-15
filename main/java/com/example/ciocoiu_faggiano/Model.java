9dedfb400e29114efd162bf0d18fafb3ef114ff1
Cioco01 <136072081+Cioco01@users.noreply.github.com>
2025-03-15 16:22:34 +0100

    Delete main directory

--- main/java/com/example/ciocoiu_faggiano/Model.java
+++
@@ -1,4 +0,0 @@
-package com.example.ciocoiu_faggiano;
-
-public class Model {
-}
--- main/java/com/example/ciocoiu_faggiano/SolveReactions.java
+++
@@ -1,9 +0,0 @@
-package com.example.ciocoiu_faggiano;
-
-public class SolveReactions {
-
-    static String solveReaction(String reaction){
-
-        return null;
-    }
-}
